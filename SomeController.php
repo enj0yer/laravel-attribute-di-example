1764dc28e02b8905d4131851e8f6456e489db09f
Enj0yer <kulagin2002ilya@gmail.com>
2024-11-07 12:53:33 +0400

    Add trait usage

--- SomeController.php
+++ SomeController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Attributes\Inject;
 use App\Services\SomeService;
+use App\Traits\CanInjectProperties;
 
 class SomeController extends Controller
 {
+    use CanInjectProperties;
+    
     #[Inject]
     private SomeService $myService;
 
